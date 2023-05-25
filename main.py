bc86b22068d8d0bafed7d9f3495bad8bbf0a594c
Cho_YongJoon <yzz1108j@gmail.com>
2023-05-25 22:14:51 +0900

    debug ended

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ import cv2 as cv
 from module_other.coordinates import *
 import module_system.game_framework as gf
 import module_system.opencv_manager as om
-from module_object.quiz import Quiz
-
-Quiz(7)
-
-exit()
 
 om.activate_opencv()
 pico2d.open_canvas(UI_WIDTH, UI_HEIGHT)
 
-gf.activate_game_framework('gaze_check_state')
+gf.activate_game_framework('play_state')
 
 om.capture.release()
 pico2d.close_canvas()
