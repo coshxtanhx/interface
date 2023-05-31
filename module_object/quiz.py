d4b8c21d30c659e2718e325f8f45b5b151f6cdf0
Cho_YongJoon <yzz1108j@gmail.com>
2023-05-31 11:16:45 +0900

    quiz updated

--- module_object/quiz.py
+++ module_object/quiz.py
@@ -5,6 +5,7 @@ from collections import deque
 import module_system.server as sv
 import module_system.stage_manager as st
 from module_other.calculation import *
+from module_object.number import int_to_string, LANG_LIST
 
 QUIZ_TYPE = {
     1: (int, CAL_ADD, int),
@@ -15,7 +16,11 @@ QUIZ_TYPE = {
     6: (CAL_SQUARE, CAL_PARENTHESE_OPEN, int, CAL_ADD, int, CAL_PARENTHESE_CLOSE),
     7: (int, CAL_MUL, CAL_PARENTHESE_OPEN, int, CAL_SUB, CAL_SQRT, int,\
         CAL_SQRT_END, CAL_PARENTHESE_CLOSE),
-    8: (CAL_SQUARE, int, CAL_SQUARE_END, CAL_SUB, int, CAL_SUB, int)
+    8: (CAL_SQUARE, int, CAL_SQUARE_END, CAL_SUB, int, CAL_SUB, int),
+    9: (CAL_SQUARE, int, CAL_MUL, int, CAL_SQUARE_END, CAL_ADD, int, CAL_SUB, int),
+    10:(CAL_PARENTHESE_OPEN, int, CAL_SUB, CAL_SQRT, int, CAL_SQRT_END,
+        CAL_PARENTHESE_CLOSE, CAL_MUL, CAL_PARENTHESE_OPEN, int, CAL_ADD, CAL_SQRT,
+        int, CAL_SQRT_END, CAL_PARENTHESE_CLOSE)
 }
 
 QUIZ_NUMBER = {
@@ -25,15 +30,31 @@ QUIZ_NUMBER = {
     4: (randint(65,99), randint(2,8), randint(3,8)**2),
     5: (randint(3,5), randint(14,74)),
     6: choice([(56,35), (30,19), (98,98), (12,24), (77,4), (72,97), (88,33)]),
-    7: (randint(12, 18), randint(10, 11), randint(6,9)**2),
-    8: (randint(14, 16), randint(85, 96), randint(83, 99)),
+    7: (randint(12,18), randint(10,11), randint(6,9)**2),
+    8: (randint(14,16), randint(85,96), randint(83,99)),
+    9: (randint(2,3), randint(2,3), randint(87,98), randint(86,99)),
+    10:(randint(5,9),) + choice([(13,6,52), (48,12,3), (44,9,11), (5,8,20)]),
 }
 
 class Quiz:
     def __init__(self, stage_num):
         self.stage_num = stage_num
-        self.string = self.create_quiz_string()
+        self.string = ''
         self.answer = 0
+        self.define_string_and_answer()
 
-    def create_quiz_string(self):
-        print(QUIZ_TYPE[self.stage_num])
+    def define_string_and_answer(self):
+        number_deque = deque(QUIZ_NUMBER[self.stage_num])
+        type_deque = deque(QUIZ_TYPE[self.stage_num])
+        formula = ''
+        for type in type_deque:
+            if type == int:
+                number = number_deque.popleft()
+                self.string += int_to_string(number, choice(LANG_LIST))
+                formula += str(number)
+                self.string 
+            else:
+                formula += type
+                self.string += sign_to_string(type)
+        self.answer = eval(formula)
+        print(self.answer, ' = ', self.string)
--- module_other/calculation.py
+++ module_other/calculation.py
@@ -1,4 +1,5 @@
 from math import *
+from random import *
 
 CAL_ADD = '+'
 CAL_SUB = '-'
@@ -7,14 +8,52 @@ CAL_DIV = '//'
 
 CAL_SQRT = 'isqrt('
 CAL_SQRT_END = ' )'
-CAL_SQUARE = 'square'
-CAL_SQUARE_END = ' )'
+CAL_SQUARE = 'square('
+CAL_SQUARE_END = ' ) '
+CAL_SPECIAL_LIST = [CAL_SQRT, CAL_SQRT_END, CAL_SQUARE, CAL_SQUARE_END]
 
 CAL_PARENTHESE_OPEN = '('
 CAL_PARENTHESE_CLOSE = ')'
+
+WORD_KOREAN, WORD_MATH = 0, 1
 
 def isqrt(n):
     return int(sqrt(n))
 
 def square(n):
     return n**2
+
+def sign_to_string(sign):
+    lang = randint(WORD_KOREAN, WORD_MATH)
+    if sign == CAL_ADD:
+        if lang == WORD_MATH:
+            return ' + '
+        return ' 더하기 '
+    elif sign == CAL_SUB:
+        if lang == WORD_MATH:
+            return ' - '
+        return ' 빼기 '
+    elif sign == CAL_MUL:
+        if lang == WORD_MATH:
+            return ' X '
+        return ' 곱하기 '
+    elif sign == CAL_DIV:
+        if lang == WORD_MATH:
+            return ' / '
+        return ' 나누기 '
+    elif sign == CAL_PARENTHESE_OPEN:
+        if lang == WORD_MATH:
+            return '('
+        return '괄호 열고 '
+    elif sign == CAL_PARENTHESE_CLOSE:
+        if lang == WORD_MATH:
+            return ')'
+        return ' 괄호 닫고'
+    elif sign == CAL_SQRT:
+        return '루트 괄호 열고 '
+    elif sign == CAL_SQUARE_END:
+        return ' 괄호 닫고의 제곱'
+    elif sign == CAL_SQRT_END:
+        return ' 괄호 닫고'
+    elif sign == CAL_SQUARE:
+        return '괄호 열고 '
